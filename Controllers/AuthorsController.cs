6fe294ee73723b30d269d0458f4ef2e0e3f02723
China3ki <limon4007@gmail.com>
2025-11-19 20:21:33 +0100

    Added new endpoints to categories

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -12,11 +12,27 @@ namespace LibraryAPI.Controllers
     public class AuthorsController(LibraryContext ctx, UploadService service) : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetAuthors()
+        public async Task<IActionResult> GetAuthors(string? sortBy, int? limit, int? start, bool desc = false)
         {
-            var authors = await ctx.Authors.Select(a => new { a.AuthorId, a.AuthorName, a.AuthorSurname }).ToListAsync();
-            if (authors.Count == 0) return NotFound();
-            else return Ok(authors);
+            var authors =  ctx.Authors.Select(a => new { a.AuthorId, a.AuthorName, a.AuthorSurname }).AsQueryable();
+
+            if (start != null) authors = authors.Skip((int)start);
+
+            if(!string.IsNullOrEmpty(sortBy))
+            {
+                authors = sortBy.ToLower() switch
+                {
+                    "id" => desc ? authors.OrderByDescending(a => a.AuthorId) : authors.OrderBy(a => a.AuthorId),
+                    "name" => desc ? authors.OrderByDescending(a => a.AuthorName) : authors.OrderBy(a => a.AuthorName),
+                    "surname" => desc ? authors.OrderByDescending(a => a.AuthorSurname)  : authors.OrderBy(a => a.AuthorSurname),
+                    _ => authors
+                };
+            }
+
+            if (limit != null) authors = authors.Take((int)limit);
+            var query = await  authors.ToListAsync();
+            if (query.Count == 0) return NotFound();
+            else return Ok(query);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetAuthor(int id)
@@ -25,8 +41,14 @@ namespace LibraryAPI.Controllers
             if (author == null) return NotFound();
             else return Ok(author);
         }
+        [HttpGet("count")]
+        public async Task<IActionResult> GetNumberOfAuthors()
+        {
+            var count = await ctx.Authors.CountAsync();
+            return Ok(count);
+        }
         [HttpPost]
-        public async Task<IActionResult> AddAuthor([Required] AuthorDto newAuthor)
+        public async Task<IActionResult> AddAuthor([FromForm][Required] AuthorDto newAuthor)
         {
             string? filepath = null;
             filepath = await service.Upload($"{newAuthor.Name} {newAuthor.Surname}", newAuthor.Image, ImageType.Author);
@@ -40,5 +62,30 @@ namespace LibraryAPI.Controllers
             await ctx.SaveChangesAsync();
             return CreatedAtAction(nameof(GetAuthor), new { id = author.AuthorId }, newAuthor);
         }
+        [HttpDelete("{authorId}")]
+        public async Task<IActionResult> DeleteAuthor(int authorId)
+        {
+            var author = await ctx.Authors.FirstOrDefaultAsync(a => a.AuthorId == authorId);
+            if (author is null) return BadRequest();
+            ctx.Remove<Author>(author);
+            await ctx.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateAuthor([FromForm] AuthorDto authorDto, [FromForm][Required] int authorId)
+        {
+            var author = await ctx.Authors.FirstOrDefaultAsync(a => a.AuthorId == authorId);
+            if (author is null) return BadRequest();
+            author.AuthorName = authorDto.Name;
+            author.AuthorSurname = authorDto.Surname;
+            if(authorDto.Image != null)
+            {
+                string filepath = await service.Upload($"{authorDto.Name} {authorDto.Surname}", authorDto.Image, ImageType.Author);
+                author.AuthorImage = filepath;
+            }
+            await ctx.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -10,11 +10,32 @@ namespace LibraryAPI.Controllers
     public class CategoriesController(LibraryContext ctx) : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetCategories()
+        public async Task<IActionResult> GetCategories(string? sortBy, int? limit, int? start, bool desc = false)
         {
-            var categories = await ctx.Categories.Select(c => new { c.CategoryId, c.CategoryName }).ToListAsync();
-            if (categories.Count == 0) return NotFound();
+            var categories =  ctx.Categories.Select(c => new { c.CategoryId, c.CategoryName }).AsQueryable();
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                categories = sortBy.ToLower() switch
+                {
+                    "id" => desc ? categories.OrderByDescending(c => c.CategoryId) : categories.OrderBy(c => c.CategoryId),
+                    "name" => desc ? categories.OrderByDescending(c => c.CategoryName) : categories.OrderBy(c => c.CategoryName),
+                    _ => categories
+                };
+            }
+
+            if (limit != null) categories = categories.Take((int)limit);
+            if (start != null) categories =categories.Skip((int)start);
+
+            var query = await categories.ToListAsync();
+            if (query.Count == 0) return NotFound();
             else return Ok(categories);
+        }
+        [HttpGet("count")]
+        public async Task<IActionResult> CountCategories()
+        {
+            var count = await ctx.Categories.CountAsync();
+            return Ok(count);
         }
         [HttpPost]
         public async Task<IActionResult> AddCategory(string name)
